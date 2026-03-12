00887ca547904df32d264d9622196cc31a6e0225
Charles Silva <suporte@charlessilva.com.br>
2026-03-12 10:23:54 -0300

    Criação do blocos realocamentos
    Criação dos blocos moved, removed, import

---
+++ AWS-blocos-moved-removed-import/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "charlessilva-novos-blocos-1"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "charlessilva-novos-blocos-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "charlessilva-novos-blocos-3"
+}
---
+++ AWS-blocos-moved-removed-import/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.36.0"
+    }
+  }
+  backend "s3" {
+    bucket = "charlessilva-s3-remote"
+    key    = "moved-removed-import/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  default_tags {
+    tags = {
+      owner      = "charlessilva"
+      managed-by = "terraform"
+    }
+  }
+}
